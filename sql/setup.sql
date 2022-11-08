19cb069f99278ba9dc4336f6c820c366dbe23906
Andrew Cornejo <atcornejo3@gmail.com>
2022-11-08 14:37:40 -0800

    creating dev branch

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,35 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS authors;
+DROP TABLE IF EXISTS books;
+
+
+
+CREATE TABLE authors (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  first_name VARCHAR,
+  last_name VARCHAR
+  dob VARCHAR
+  pob VARCHAR
+);
+
+CREATE TABLE books (
+  id BIGINT GENERATED ALWAYS AS IDENTITY,
+  title VARCHAR
+  released YEAR
+);
+
+INSERT INTO authors (
+  first_name,
+  last_name,
+  dob,
+  pob
+)
+VALUES 
+  ('Moira', 'Rose', 'example'),
+
+;
+
+INSERT INTO books (
+\
+)
